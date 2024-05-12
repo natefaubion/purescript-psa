a40a9bf63caabc7946cec6423f545c696fe844c2
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2024-05-12 13:01:21 +0530

    changed the library structure in publishing.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import pkg from './package.json' assert { type: 'json' };
-import * as Main from './output/Main/index.js';
+import * as Main from './dist/index.js';
 global.version = pkg.version;
 Main.main();
